b7ef34599d7beaa52e3877281d7c1f98768ad30a
almathaler <athaler00@stuy.edu>
2019-12-11 01:10:52 -0500

    put functions.c back to originally working state, whitespace is so weird, checker changes after stersep?? input=checker, the parsed version of input, and then strsep(&input, delim) makes checker strseps output

--- whitespace_test.c
+++ whitespace_test.c
@@ -32,8 +32,6 @@ char ** parse_input(char *input, char *delimiter){
   char checker[256];
   strcpy(checker, input);
   printf("checker[0]:%c\tchecker[1]:%c\tchecker[2]:%c\n", checker[0], checker[1], checker[2]);
-  //char delims[256] = *delimiter;
-  //printf("delims[0]:%c\tdelims[1]:%c\tdelims[2]:%c\n", delims[0], delims[1], delims[2]);
   int i = 0;
   while(checker != NULL && *checker != '\0'){
     //check for leading whitespace
@@ -50,27 +48,43 @@ char ** parse_input(char *input, char *delimiter){
       not_null++;
     }
     printf("not_null: %d\n", not_null);
-    checker[index_checker + not_null] = '\0';
+    checker[not_null] = '\0';
     printf("(after leading) checker now: %s\n", checker);
     //check for trailing whitespace
-    index_checker = strlen(checker); //at end, "abc\0" will be null
-    while(checker[index_checker] > 0 &&  //while you're not going past the string and also going only thru whitespace
-        (checker[index_checker] == '\n' ||
-        checker[index_checker] == '\t' ||
-        checker[index_checker] == ' ')){
-          index_checker--;
-        }
-    checker[index_checker] = '\0';
+    index_checker = 0;
+    int other_index = -1;
+    while(checker[index_checker] != '\0'){
+       if(checker[index_checker] != ' ' && checker[index_checker] != '\t' && checker[index_checker] != '\n'){
+           other_index = index_checker;
+       }
+       index_checker++;
+    }
+    checker[other_index + 1] = '\0';
     printf("(after trailing) checker now: \"%s\"\n", checker);
-    //
-    //strcpy(strsep_checker, checker); //bc strsep needs a string literal
     input = checker;
-    printf("seg fault after strcpy?\n");
+
+    printf("seg fault after input = checker?\n");
+    printf("input before strsep: \"%s\"\n", input);
+
     to_return[i] = strsep(&input, delimiter);
+
     printf("seg fault after strsep?\n");
-    strcpy(checker, input); //so checker gets the changes
-    //printf("seg fault after 2nd strcpy?\n");
-    //checker = *input;
+    printf("checker: \"%s\", %p\tinput: \"%s\", %p\n", checker, &checker, input, &input);
+    printf("to_return[%d]: \"%s\"\tchecker: \"%s\"\n", i, to_return[i], checker);
+    printf("modification:\n");
+    //set checker equal to input, HOW????? literally spent 3 hrs on THIS
+    char buffer[256] = "\0";
+    printf("seg fault after assigning buffer?\n");
+    if (input!= NULL && *input != '\0'){
+      strcpy(buffer, input);
+    }
+    int k = 0;
+    while(buffer[k]!='\0'){
+      checker[k] = buffer[k];
+      k++;
+    }
+    checker[k] = '\0';
+    //
     printf("checker: \"%s\", %p\tinput: \"%s\", %p\n", checker, &checker, input, &input);
     printf("to_return[%d]: \"%s\"\tchecker: \"%s\"\n", i, to_return[i], checker);
     i++;
