3040e0a5861b932fa7408c85768e0905da9752e5
Hilary Zen <hzen00@stuy.edu>
2019-12-12 02:30:03 -0500

    fixed

--- functions.c
+++ functions.c
@@ -27,7 +27,7 @@ int execute_type(char *input){
       less_than(input);
       break;
     case 4:
-      //pipe();
+      pipe(input);
       break;
     case 5:
       double_greater_than(input);
